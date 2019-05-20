979270fe15fe1c849722ccc07ed6e44ba1fdb18b
mhmai <mhm@wemanity.com>
2019-05-20 13:41:20 +0200

    fix log.error instead of log.info

--- src/plugins/container/handlers.js
+++ src/plugins/container/handlers.js
@@ -58,7 +58,7 @@ exports.start = {
 			await containerSrvc.start(request.params.image, org)
 			log.info(`END start container ${org}`)
 		} catch (error) {
-			log.info(`ERROR start container: ${error}`)
+			log.error(`ERROR start container: ${error}`)
 			return h.response(error.message).code(400)	
 		}
 
